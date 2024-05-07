afe13f55ed35107554266bd1f7c841c32ad64d20
Daniel Fiedler <daniel.fiedler@grupoboticario.com.br>
2024-05-07 11:28:16 -0300

    feat: add new image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="mt-10 flex min-h-screen flex-col items-center justify-between">
+      <div className="w-full m-10">
+        <img
+          alt="sou uma imagem legal"
+          className="w-full"
+          src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
+        />
+      </div>
       <p className="p-24 fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         Poc Webvitals
       </p>
