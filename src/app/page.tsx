20f6d0886b3abe9954daf8cd2b13ec3b5f583b3b
Daniel Fiedler <daniel.fiedler@grupoboticario.com.br>
2024-05-07 11:33:30 -0300

    feat: replace img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <p className="p-24 fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         Poc Webvitals
       </p>
-      <div className="w-full m-10">
+      <div className="w-50 m-10 m-auto mt-10">
         <img
           alt="sou uma imagem legal"
           className="w-full"
